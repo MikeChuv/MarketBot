8e856b8e02e56de7637584cf8ac37953b5b247bb
MikeChuv <mchuvorkin@gmail.com>
2020-09-19 21:59:33 +0300

    ekatalog test

--- cmds.py
+++ cmds.py
@@ -4,44 +4,86 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
 import requests
 import parsingLib
+import re
+import urllib.parse
 
-def cmd_img(imgs, event, vk_session):
-    vk = vk_session.get_api()
-    attachments = []
-    for img in imgs:
-        imgSource = img.find('img').get('src')
-        imgSource = "https:" + imgSource
-        upload = VkUpload(vk_session)
-        image = requests.get(imgSource, stream=True)
-        photo = upload.photo_messages(photos=image.raw)[0]
-        attachments.append(
-            'photo{}_{}'.format(photo['owner_id'], photo['id'])
-        )
-    vk.messages.send(
-        user_id=event.user_id,
-        attachment=','.join(attachments),
-        message='Фото товара',
-        random_id=get_random_id()
-    )
+
+def prices_soup(soup):
+    price_page = soup.find("div", {'class': 'cia-cs'})
+    price_page = price_page.find('a').get('href')
+    price_page = 'https://market.yandex.ru' + price_page
+    print(price_page)
+    price_soup = parsingLib.makeSoup(price_page)
+    return price_soup
 
 
 def get_price(soup):
-    price_page = soup.find("li", {'data-name': 'offers'})
-    price_page = price_page.find('a').get('href')
-    price_page = 'https://market.yandex.ru' + price_page
-    # print(pricePage)
-    price_soup = parsingLib.makeSoup(price_page)
+    price_soup = prices_soup(soup)
     prices = parsingLib.allDIV(price_soup, "price")
     return prices
 
 
+def get_shop(soup):
+    shop_soup = prices_soup(soup)
+    shops = shop_soup.findAll("a", {'class': 'n-shop-logo'})
+    return shops
+
+
+def cmd_img(soup, imgs, event, vk_session):
+    vk = vk_session.get_api()
+    attachments = []
+    main_img = soup.find("div", {'class': 'img200'})
+    main_img_src = "https://www.e-katalog.ru" + re.search(r'/jpg_zoom1/([0-9])+', str(main_img)).group(0) + ".jpg"
+    upload = VkUpload(vk_session)
+    main_image = requests.get(main_img_src, stream=True)
+    main_photo = upload.photo_messages(photos=main_image.raw)[0]
+    attachments.append('photo{}_{}'.format(main_photo['owner_id'], main_photo['id']))
+    print(main_img_src)
+    for img in imgs:
+        img_source = re.search(r'https://mzimg.com/big(?:[a-zA-Z]|[0-9]|[/.-_])+', str(img)).group(0)
+        image = requests.get(img_source, stream=True)
+        photo = upload.photo_messages(photos=image.raw)[0]
+        attachments.append('photo{}_{}'.format(photo['owner_id'], photo['id']))
+    vk.messages.send(
+        user_id=event.user_id,
+        attachment=','.join(attachments),
+        message='Фото товара',
+        random_id=get_random_id())
+
+
 def cmd_price(soup, event, vk_session):
     vk = vk_session.get_api()
-    vk.messages.send(user_id=event.user_id, message='Получаю цены',
+    vk.messages.send(user_id=event.user_id,
+                     message='Получаю цены',
                      random_id=get_random_id())
-    myPrices = get_price(soup)
-    myPrices1 = []
-    for myPrice in myPrices:
-        myPrices1.append('\n' + myPrice.text)
-    vk.messages.send(user_id=event.user_id, message=myPrices1,
+    my_prices = get_price(soup)
+    my_prices1 = []
+    my_shops = get_shop(soup)
+    print(my_shops)
+    for my_shop in my_shops:
+        print(my_shop.find('img').get('alt'))
+    i = 1
+    for my_price in my_prices:
+        my_prices1.append('\n' + my_price.text)
+    # for my_shop in my_shops:
+    #     my_prices1.insert(i, my_shop.find('img').get('alt'))
+    #     i += 2
+
+    vk.messages.send(user_id=event.user_id,
+                     message=my_prices1,
                      random_id=get_random_id())
+
+
+def cmd_search_get_url(event, vk_session):
+    vk = vk_session.get_api()
+    message_list = event.text.split()
+    del message_list[0]
+    word_string = ""
+    for word in message_list:
+        word_encoded = urllib.parse.quote_plus(word)
+        word_string = word_string + "+" + word_encoded  # сохраняет плюс перед первым словом
+    search_url = "https://www.e-katalog.ru/ek-list.php?search_=" + word_string
+    vk.messages.send(user_id=event.user_id,
+                     message=search_url,
+                     random_id=get_random_id())
+    return search_url
--- script.py
+++ script.py
@@ -8,7 +8,8 @@ from vk_api import VkUpload
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
 import cmds
-
+import re
+import urllib.parse
 
 
 
@@ -17,7 +18,7 @@ url = ""
 # attachments = []
 
 # soup = parsingLib.makeSoup(url)
-workbook = xlsxwriter.Workbook("prices.xlsx")                # создаем новый Excel-документ
+workbook = xlsxwriter.Workbook("prices.xlsx")               # создаем новый Excel-документ
 worksheet = workbook.add_worksheet()                        # создаем в нем новый лист
 # imgs = parsingLib.allDIV(soup, "n-gallery__item")
 vk_session = vk_api.VkApi(token='3d845b2a4ef1487db3289577b38b1ceca74997182e00470743e8166e3f8b52d459bf89183f55e5aabcc53')
@@ -32,42 +33,61 @@ opts = {
     "cmds": {
         "tbr": ('покажи', 'сколько', 'скинь', 'расскажи'),
         "cIMG": ('скинь картинки', 'фото', 'покажи картинки'),
-        "cURL": ('держи ссылку', 'вот это'),
         "cPrice": ('сколько стоит', 'какая цена', 'цена', 'стоимость'),
-        "cShop": ('где продают', 'где купить', 'магазины')
+        "cShop": ('где продают', 'где купить', 'магазины'),
+        "cSearch": ('найди', 'поиск')
     }
 
 }
 
 
 for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+    tracked_item = False
+    search_url = ""
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me:  # and event.text:
         # Слушаем longpoll, если пришло сообщение то:
-        print(event.text[8:24])
         if event.text == 'Привет' or event.text == 'привет':
             if event.from_user:
-                vk.messages.send(user_id=event.user_id, message='Привет!\n Напиши мне то, что ты хочешь',
+                vk.messages.send(user_id=event.user_id,
+                                 message='Привет!\n Напиши мне то, что ты хочешь',
                                  random_id=get_random_id())
 
-            elif event.from_chat:
-                vk.messages.send(chat_id=event.chat_id, message='Ваш текст', random_id=get_random_id())
+            # elif event.from_chat:
+            #     vk.messages.send(chat_id=event.chat_id, message='Ваш текст', random_id=get_random_id())
 
         for keys, values in opts["cmds"].items():
             for value in values:
-                if event.text == value:
-                    if keys == "cPrice":
-                        cmds.cmd_price(soup, event, vk_session)
+                if len(event.text.split()) != 0:
+                    if event.text.split()[0] == value:
+                        if keys == "cPrice":
+                            cmds.cmd_price(soup, event, vk_session)
+                        if keys == "cIMG":
+                            cmds.cmd_img(soup, imgs, event, vk_session)
+                        if keys == "cSearch":
+                            search_url = cmds.cmd_search_get_url(event, vk_session)
 
-                    if keys == "cIMG":
-                        cmds.cmd_img(imgs, event, vk_session)
-
-        if event.text[8:24] == 'market.yandex.ru':
+        # event.attachments - это словарь с вложениями
+        # for key, value in event.attachments.items():
+        #     print(key, value)
+        if (event.text[12:24] == 'e-katalog.ru') or (event.attachments.get("attach1_type") == 'link'):
             if event.from_user:
-                vk.messages.send(user_id=event.user_id, message='Ссылка получена',
+                vk.messages.send(user_id=event.user_id,
+                                 message='Ссылка получена',
                                  random_id=get_random_id())
-            url = event.text
+            if len(event.attachments) == 0:
+                url = event.text
+            else:
+                url = event.attachments.get("attach1_url")
             soup = parsingLib.makeSoup(url)
-            imgs = parsingLib.allDIV(soup, "n-gallery__item")
+            tracked_item = True
+            imgs = soup.find_all(onclick=re.compile("mzimg.com/big"), attrs={'class': 'i15-item'})
+            prices = soup.find_all(link=re.compile("/ek-item.php\?resolved_name_"), title="Сравнить цены!")
+            for price in prices:
+                price_page_url = 'https://www.e-katalog.ru' + price['link']
+                price_page_soup = parsingLib.makeSoup(price_page_url)
+                some_prices = price_page_soup.find_all(attrs={'class': re.compile("shop-([0-9])+")})
+                for some_price in some_prices:
+                    print(str(some_price.find('a').get('onmouseover')).split('\"')[1])
 
 # переписать этот блок в место со словарем и добавить в словарь команду для картинок
 # добавить модуль нечеткого сравнения для комманд словаря
